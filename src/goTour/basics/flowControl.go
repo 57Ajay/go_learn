8902691ad482b24a17c6eafb237114775723e151
57Ajay <57ajay.u@gmail.com>
2025-01-20 23:33:43 +0530

    only 1 left in basics

--- src/goTour/basics/flowControl.go
+++ src/goTour/basics/flowControl.go
@@ -2,7 +2,10 @@ package basics
 
 import (
 	"fmt"
+	"io"
 	"math"
+	"os"
+	"path/filepath"
 	"runtime"
 	"time"
 )
@@ -63,10 +66,86 @@ func getWeekday() string {
 
 }
 
+func deferFunc() {
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		// Will print in reverse order
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+}
+
+func CopyFile(dstName, srcName string) (written int64, err error) {
+
+	// Some extra code to get the current directory
+	executablePath, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Executable Path:", executablePath)
+
+	// Get the directory containing the executable:
+	executableDir := filepath.Dir(executablePath)
+	fmt.Println("Executable Directory:", executableDir)
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Current Working Directory:", currentDir)
+
+	// Main code for the function
+	src, err := os.Open(srcName)
+	if err != nil {
+		return
+	}
+	defer src.Close()
+
+	dst, err := os.Create(dstName)
+	if err != nil {
+		return
+	}
+	defer dst.Close()
+
+	return io.Copy(dst, src)
+}
+
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
+}
+
+func panicDefer() {
+	f()
+	fmt.Println("Returned normally from f.")
+}
 func FlowControlMain() {
 	fmt.Println("for/while loop: ", forLoop())
 	fmt.Println("power: ", pow(3, 5, 100))
 	fmt.Println("sqrt: ", sqrt(9))
 	fmt.Println("OS: ", getOS())
-	println("When is Saturday? ", getWeekday())
+	defer fmt.Println("defered function")
+	fmt.Println("When is Saturday? ", getWeekday())
+	deferFunc()
+	fmt.Println(CopyFile("test.txt", "main.go"))
+	panicDefer()
 }
---
+++ src/goTour/basics/types.go
@@ -0,0 +1,18 @@
+package basics
+
+import (
+	"fmt"
+)
+
+func TypesMain() {
+	fmt.Println("Go's basic types are:")
+	fmt.Println("bool")
+	fmt.Println("string")
+	fmt.Println("int  int8  int16  int32  int64")
+	fmt.Println("uint uint8 uint16 uint32 uint64 uintptr")
+	fmt.Println("byte // alias for uint8")
+	fmt.Println("rune // alias for int32")
+	fmt.Println("     // represents a Unicode code point")
+	fmt.Println("float32 float64")
+	fmt.Println("complex64 complex128")
+}
