5d5f48b574c893b991399637d87c8a0723b6614c
57Ajay <57ajay.u@gmail.com>
2025-02-21 21:39:56 +0530

    IP-Done

--- goMore/src/netPackage/ip.go
+++ goMore/src/netPackage/ip.go
@@ -36,12 +36,61 @@ func parseIp() {
 	fmt.Println("Manual IPv4 - To4():", ip4Manual.To4())
 	fmt.Println("Manual IPv4 - To16():", ip4Manual.To16())
 	ip4Correct := net.IP(net.CIDRMask(32, 32))
-	copy(ip4Correct[12:16], ip4Manual)
+	// copy(ip4Correct[12:16], ip4Manual)
 	fmt.Println("Corrected:", ip4Correct, "To 4:", ip4Correct.To4())
 
+	// Join host and port
+	addr := net.JoinHostPort("192.168.1.1", "8080")
+	fmt.Println("Joined address:", addr) // Output: Joined address: 192.168.1.1:8080
+
+	// Split host and port
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		fmt.Println("Error splitting:", err)
+		return
+	}
+	fmt.Println("Host:", host) // Output: Host: 192.168.1.1
+	fmt.Println("Port:", port) // Output: Port: 8080
+
+	// Example of incorrect usage, missing port
+	_, _, err = net.SplitHostPort("192.168.1.1")
+	if err != nil {
+		fmt.Println("Error splitting:", err) // Output: Error splitting: address 192.168.1.1: missing port in address
+	}
+
+}
+
+func resolveIp() {
+	// This looks for ip address for a host name; hostname -> IP address
+	ips, err := net.LookupIP("google.com")
+	if err != nil {
+		fmt.Println("Error looking up IP address:", err)
+		return
+	}
+	for _, ip := range ips {
+		fmt.Println("IP address:", ip)
+	}
+
+	// IP address -> hostname
+	names, err := net.LookupAddr("8.8.8.8")
+	if err != nil {
+		fmt.Println("Error looking up hostname:", err)
+	}
+	for _, name := range names {
+		fmt.Println("Hostname:", name)
+	}
+
+	// LookUp port
+	port, err := net.LookupPort("tcp", "https")
+	if err != nil {
+		fmt.Println("Error looking up port:", err)
+	}
+	fmt.Println("Port:", port)
 }
 
 func IPMain() {
 	fmt.Println("This is the IPMain function")
 	parseIp()
+	fmt.Print("\n -------resolveIp-------\n")
+	resolveIp()
 }
